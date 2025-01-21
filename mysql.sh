815bac82ef1b883d7a2efc031ce2ae9b01a625ed
hariprasadshetti <shettihari133@gmail.com>
2025-01-21 15:52:22 +0530

    added logs folder

--- mysql.sh
+++ mysql.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/expense-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$(basename "$0" | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/-$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo -e "$2......$R failure"
-    exit 1
-else
-    echo -e "$2.......$G success"
-fi
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2......$R failure \e[0m"
+        exit 1
+    else
+        echo -e "$2.......$G success \e[0m"
+    fi
 }
 
-CHECK_ROOT(){
-
-if [ $USERID -ne 0 ]
-then  
-    echo "Error:throw the error message user does not have root access"
-    exit 1
-fi
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]; then  
+        echo -e "${R}Error: User does not have root access. Please run as root or use sudo.${Y}"
+        exit 1
+    fi
 }
-echo "script started at and excuted at: $TIMESTAMP"
 
+echo "Script started and executed at: $TIMESTAMP"
+
+# Ensure log directory exists
 mkdir -p "$LOGS_FOLDER"
 
 CHECK_ROOT
 
-dnf install mysql-server -y &>>$LOG_FILE_NAME
-VALIDATE $? "Installing Mysql-sever"
+dnf install mysql-server -y &>>"$LOG_FILE_NAME"
+VALIDATE $? "Installing MySQL server"
 
-systemctl enable mysqld &>>$LOG_FILE_NAME
-VALIDATE $? "Enableing Mysql-sever"
+systemctl enable mysqld &>>"$LOG_FILE_NAME"
+VALIDATE $? "Enabling MySQL server"
 
-systemctl start mysqld &>>$LOG_FILE_NAME
-VALIDATE $? "Starting Mysql-server"
+systemctl start mysqld &>>"$LOG_FILE_NAME"
+VALIDATE $? "Starting MySQL server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>"$LOG_FILE_NAME"
 VALIDATE $? "Setting up root password"
-
-
